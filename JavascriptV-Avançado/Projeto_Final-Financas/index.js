319dd2e014c71c3853ef995570a8be782d837e78
Pedro Santana <pedro.ferraz01@gmail.com>
2022-12-31 15:38:00 -0300

    Finishing the final project

--- JavascriptV-Avançado/Projeto_Final-Financas/index.js
+++ JavascriptV-Avançado/Projeto_Final-Financas/index.js
@@ -7,24 +7,23 @@ function renderTransactions(transactions) {
   //Create transaction reason
   const reason = document.createElement("h3");
   reason.classList.add("transaction-reason");
-  reason.textContent = `${transactions?.id}-->${transactions?.reason}`;
+  reason.textContent = `${transactions?.reason}`;
   //Create transaction value
   const value = document.createElement("h3");
   value.classList.add("transaction-value");
-  value.textContent = `R$: ${transactions?.value}`;
+  value.textContent = `${transactions?.value}`;
   //Create edit icon
   const edit = new Image(20, 20);
   edit.src = "./img/editar.png";
   edit.id = "edit-icon";
+  //Edit event
+  edit.addEventListener("click", editTransaction);
   //Create delete icon
   const delet = new Image(20, 20);
   delet.src = "./img/escluir.png";
   delet.id = "delet-icon";
-  //Delete function
-  delet.addEventListener("click", (ev) => {
-    const target = ev.currentTarget.parentNode.parentNode;
-    console.log(target);
-  });
+  //Delete event
+  delet.addEventListener("click", deleteTransaction);
   value.append(edit, delet);
   //Put all 2 at the div
   transaction.append(reason, value);
@@ -41,6 +40,7 @@ async function fetchTransGET() {
   transactions.forEach(valueUpdate);
 }
 document.addEventListener("DOMContentLoaded", () => fetchTransGET());
+document.addEventListener("DOMContentLoaded", () => valueUpdate());
 
 //async function to POST
 form = document.querySelector("form");
@@ -66,10 +66,79 @@ form.addEventListener("submit", async (ev) => {
   }
 });
 
+//async function to DELETE
+async function transactionDelete(transctionDeleted) {
+  //Deletar do banco de dados
+  await fetch(`http://localhost:3000/transactions/${transctionDeleted.id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(transctionDeleted),
+  });
+  //Atualizado o valor
+  valueUpdate(transctionDeleted);
+}
+
+//Delete function
+function deleteTransaction(ev) {
+  //Get the target
+  const target = ev.currentTarget.parentNode.parentNode;
+  const id = ev.currentTarget.parentNode.parentNode.id.match(/\d+/gm);
+  //Create a object to exclude
+  const transctionDeleted = {
+    id: parseInt(id),
+    value: ev.currentTarget.parentNode.textContent * -1,
+  };
+  //Deleting from html
+  const element = document.querySelector(".transactions");
+  element.removeChild(target);
+  //Call function to DELETE
+  transactionDelete(transctionDeleted);
+}
+
+//async function to PATCH
+async function transactionPatch(transactionPatch) {
+  await fetch(`http://localhost:3000/transactions/${transactionPatch.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(transactionPatch),
+  });
+}
+//Function to PATCH
+function editTransaction(ev) {
+  //Get the target
+  const target = ev.currentTarget.parentNode.parentNode;
+  //Edit reason and value
+  const editReason = prompt("New Reason:");
+  const editValue = prompt("New Value");
+  if (editReason === "" || editValue === "") {
+  } else {
+    const editedTransaction = {
+      id: ev.currentTarget.parentNode.parentNode.id.match(/\d+/gm),
+      reason: editReason,
+      value: editValue,
+    };
+    //Edit reason and value html
+    target.firstChild.textContent = editedTransaction.reason;
+    target.lastChild.textContent = editedTransaction.value;
+
+    //PATCH at DB
+    transactionPatch(editedTransaction);
+
+    document.location.reload(true);
+  }
+}
 //Function to get and maintain the value updated
 let totalValue = 0;
-function valueUpdate(transaction) {
+function valueUpdate(transaction = { value: "0" }) {
   const value = document.getElementById("amount");
   totalValue += parseFloat(transaction.value);
-  value.textContent = `Amount:R$${totalValue}`;
+  if (totalValue === 0) {
+    value.textContent = `Amount:R$0`;
+  } else {
+    value.textContent = `Amount:R$${totalValue}`;
+  }
 }
